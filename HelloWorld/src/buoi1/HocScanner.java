8625edcb641095ebf4058725c26194cdee6df986
Van Nam <toanhocabc29@gmail.com>
2026-04-18 10:38:34 +0700

    update Chương trình đầu tiên

---
+++ HelloWorld/src/buoi1/HocScanner.java
@@ -0,0 +1,17 @@
+package buoi1;
+
+import java.util.Scanner;
+
+public class HocScanner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nhập số nguyên: ");
+		int a = sc.nextInt();
+		System.out.println("Nhập số thực")
+		;
+		
+	}
+
+}
